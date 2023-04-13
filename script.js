e74295c609d149a8085a829bc9d0f5a61a300fed
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-04-13 21:05:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
 //your JS code here. If required.
-var heading = document.createElement("h1");
-heading.innerText = "DOM load success";
-
-document.body.appendChild(heading);
+document.querySelector("body").innerHTML = "DOM load success";
